4cefef2b3651e639ee4962fc230415094667e23c
nataliamedinat <nataliamedinabotero@gmail.com>
2020-01-29 20:01:42 +0000

    Pep8

--- api/v1/app.py
+++ api/v1/app.py
@@ -13,20 +13,20 @@ app.register_blueprint(app_views)
 def close(c):
     storage.close()
 
+
 @app.errorhandler(404)
 def not_found(e):
     return make_response(jsonify({'error': 'Not found'}), 404)
 
 
 if __name__ == "__main__":
-    if getenv('HBNB_API_HOST') == None:
+    if getenv('HBNB_API_HOST') is None:
         host = "0.0.0.0"
     else:
         host = getenv('HBNB_API_HOST')
 
-    if getenv('HBNB_API_PORT') == None:
+    if getenv('HBNB_API_PORT') is None:
         port = "5000"
     else:
         port = getenv('HBNB_API_PORT')
         app.run(host=host, port=port, threaded=True, debug=True)
-
