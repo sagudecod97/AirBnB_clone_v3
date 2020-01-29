967a3ed0c30de0e23886e2a2b7ed831226b2fcbd
Santiago <sagudecod97@gmail.com>
2020-01-29 20:24:26 +0000

    Fixed OS import

--- api/v1/app.py
+++ api/v1/app.py
@@ -3,7 +3,7 @@
 from flask import Flask, Blueprint, jsonify, make_response
 from models import storage
 from api.v1.views import app_views
-from os import getenv
+import os
 
 app = Flask(__name__)
 app.register_blueprint(app_views)
