0c472f328209b188514b89eb0fe48e78e9c19652
nataliamedinat <nataliamedinabotero@gmail.com>
2020-01-29 20:28:33 +0000

    Fixed pep8

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -4,9 +4,11 @@ from api.v1.views import app_views
 from flask import jsonify
 from models import storage
 
+
 @app_views.route('/status', methods=['GET'])
 def get_status():
     return jsonify({"status": "OK"})
+
 
 @app_views.route('/stats', methods=['GET'])
 def get_stats():
@@ -18,7 +20,3 @@ def get_stats():
         "states": storage.count("State"),
         "users": storage.count("User")
     })
-    #for key in objects.keys():
-        #objects[key] = key[0].upper()+key[1:-1]storage.count(key[0].upper()+key[1:-1])
-   # return jsonify(objects)
-
