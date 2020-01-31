93e12d1802081edea25a7a279c549eab205f95d6
nataliamedinat <nataliamedinabotero@gmail.com>
2020-01-31 00:30:50 +0000

    Import CORS

--- api/v1/app.py
+++ api/v1/app.py
@@ -4,6 +4,8 @@ from flask import Flask, Blueprint, jsonify, make_response
 from models import storage
 from api.v1.views import app_views
 import os
+from flask_cors import CORS
+
 
 app = Flask(__name__)
 app.register_blueprint(app_views)
